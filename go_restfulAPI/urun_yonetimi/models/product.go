e80a91ad7eae4e34cdf31c705bdd8a2caeee0683
Sevgi Fidan <60657783+SevgiF@users.noreply.github.com>
2022-11-29 16:23:27 +0300

    Revert "Swagger/new branc"

--- go_restfulAPI/urun_yonetimi/models/product.go
+++ go_restfulAPI/urun_yonetimi/models/product.go
@@ -2,24 +2,10 @@ package models
 
 import "time"
 
-// Represents the product for this app
-// swagger:model
 type Product struct {
-	// the id for product
-	// required: true
-	// unique: true
-	ID int `json:"id"`
-	// the name for product name
-	// required: true
-	// min lenght: 5
-	Name string `json:"name"`
-	// the description for product description
-	// required: false
-	Description string `json:"description"`
-	// the product creation time
-	// example: 2022-01-01
-	CreatedOn time.Time `json:"createdon"`
-	// the product editing time
-	// example: 2022-01-01
-	ChangedOn time.Time `json:"changedon"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedOn   time.Time `json:"createdon"`
+	ChangedOn   time.Time `json:"changedon"`
 }
